a700c9610f6621c7170e364effececf01464f732
Katrinerosa <katrinerosa@gmail.com>
2025-02-07 11:31:36 +0100

    forstår ikke helt hvorfor sder er kommet tallet tre

---
+++ destination.js
@@ -0,0 +1,6 @@
+
+let search = window.location.search
+let params = new URLSearchParams(search)
+console.log(params);
+let id = params.get("id")
+console.log(id);
---
+++ index.js
@@ -0,0 +1,16 @@
+fetch("/data/destinations.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let sectionElm = document.createElement("section");
+
+    sectionElm.innerHTML = data.destinations
+      .map(
+        (destination) =>
+          ` <img src="/img/${destination.image}"></img> 
+        <a href="destination.html?id=>${destination.id}" >
+        ${destination.title}
+        </a>`
+      )
+      .join("");
+    document.querySelector("#root").append(sectionElm);
+  });
